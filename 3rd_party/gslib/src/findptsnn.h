1b4c5ee09d924c24aeddd6c1534ca4d0898fee0c
Ketan Mittal <kmittal2@illinois.edu>
2017-07-01 17:49:29 -0700

    add findptsnn files

---
+++ 3rd_party/gslib/src/findptsnn.h
@@ -0,0 +1,75 @@
+#ifndef FINDPTSNN_H
+#define FINDPTSNN_H
+
+#if !defined(COMM_H)
+#warning "findptsnn.h" requires "comm.h"
+#endif
+
+#define findptsnn_setup_2   PREFIXED_NAME(findptsnn_setup_2)
+#define findptsnn_free_2    PREFIXED_NAME(findptsnn_free_2 )
+#define findptsnn_2         PREFIXED_NAME(findptsnn_2      )
+#define findptsnn_eval_2    PREFIXED_NAME(findptsnn_eval_2 )
+#define findptsnn_setup_3   PREFIXED_NAME(findptsnn_setup_3)
+#define findptsnn_free_3    PREFIXED_NAME(findptsnn_free_3 )
+#define findptsnn_3         PREFIXED_NAME(findptsnn_3      )
+#define findptsnn_eval_3    PREFIXED_NAME(findptsnn_eval_3 )
+
+struct findptsnn_data_2;
+struct findptsnn_data_3;
+
+struct findptsnn_data_2 *findptsnn_setup_2(
+  const struct comm *const comm,
+  const double *const elx[2],
+  const unsigned n[2], const uint nel,
+  const unsigned m[2], const double bbox_tol,
+  const uint local_hash_size, const uint global_hash_size,
+  const unsigned npt_max, const double newt_tol, const sint *const nsid);
+
+struct findptsnn_data_3 *findptsnn_setup_3(
+  const struct comm *const comm,
+  const double *const elx[3],
+  const unsigned n[3], const uint nel,
+  const unsigned m[3], const double bbox_tol,
+  const uint local_hash_size, const uint global_hash_size,
+  const unsigned npt_max, const double newt_tol, const sint *const nsid);
+
+void findptsnn_free_2(struct findptsnn_data_2 *fd);
+void findptsnn_free_3(struct findptsnn_data_3 *fd);
+
+void findptsnn_2(    uint   *const  code_base   , const unsigned  code_stride   ,
+                   uint   *const  proc_base   , const unsigned  proc_stride   ,
+                   uint   *const    el_base   , const unsigned    el_stride   ,
+                   double *const     r_base   , const unsigned     r_stride   ,
+                   double *const dist2_base   , const unsigned dist2_stride   ,
+             const double *const     x_base[2], const unsigned     x_stride[2],
+    const uint  *const  session_id_base , const unsigned  session_id_stride   ,
+             const uint npt, struct findptsnn_data_2 *const fd);
+
+void findptsnn_3(    uint   *const  code_base   , const unsigned  code_stride   ,
+                   uint   *const  proc_base   , const unsigned  proc_stride   ,
+                   uint   *const    el_base   , const unsigned    el_stride   ,
+                   double *const     r_base   , const unsigned     r_stride   ,
+                   double *const dist2_base   , const unsigned dist2_stride   ,
+             const double *const     x_base[3], const unsigned     x_stride[3],
+  const uint *const  session_id_base   , const unsigned    session_id_stride   ,
+             const uint npt, struct findptsnn_data_3 *const fd);
+
+void findptsnn_eval_2(
+        double *const  out_base, const unsigned  out_stride,
+  const uint   *const code_base, const unsigned code_stride,
+  const uint   *const proc_base, const unsigned proc_stride,
+  const uint   *const   el_base, const unsigned   el_stride,
+  const double *const    r_base, const unsigned    r_stride,
+  const uint npt,
+  const double *const in, struct findptsnn_data_2 *const fd);
+ 
+void findptsnn_eval_3(
+        double *const  out_base, const unsigned  out_stride,
+  const uint   *const code_base, const unsigned code_stride,
+  const uint   *const proc_base, const unsigned proc_stride,
+  const uint   *const   el_base, const unsigned   el_stride,
+  const double *const    r_base, const unsigned    r_stride,
+  const uint npt,
+  const double *const in, struct findptsnn_data_3 *const fd);
+
+#endif
---
+++ 3rd_party/gslib/src/findptsnn_local.h
@@ -0,0 +1,102 @@
+#ifndef FINDPTSNN_LOCAL_H
+#define FINDPTSNN_LOCAL_H
+
+#if !defined(MEM_H) || !defined(FINDPTSNN_EL_H) || !defined(OBBOX_H)
+#warning "findptsnn_local.h" requires "mem.h", "findpnn_el.h", "obbox.h"
+#endif
+
+#define findptsnn_local_setup_2   PREFIXED_NAME(findptsnn_local_setup_2)
+#define findptsnn_local_free_2    PREFIXED_NAME(findptsnn_local_free_2 )
+#define findptsnn_local_2         PREFIXED_NAME(findptsnn_local_2      )
+#define findptsnn_local_eval_2    PREFIXED_NAME(findptsnn_local_eval_2 )
+
+struct findptsnn_local_hash_data_2 {
+  uint hash_n;
+  struct dbl_range bnd[2];
+  double fac[2];
+  uint *offset;
+  uint max;
+};
+
+struct findptsnn_local_data_2 {
+  unsigned ntot;
+  const double *elx[2];
+  const int *session_id;
+  struct obbox_2 *obb;
+  struct findptsnn_local_hash_data_2 hd;
+  struct findptsnn_el_data_2 fed;
+  double tol;
+};
+
+void findptsnn_local_setup_2(struct findptsnn_local_data_2 *const fd,
+                           const double *const elx[3],
+                           const int *const session_id,
+                           const unsigned n[2], const uint nel,
+                           const unsigned m[2], const double bbox_tol,
+                           const uint max_hash_size,
+                           const unsigned npt_max, const double newt_tol);
+void findptsnn_local_free_2(struct findptsnn_local_data_2 *const fd);
+void findptsnn_local_2(
+        uint   *const  code_base   , const unsigned  code_stride   ,
+        uint   *const    el_base   , const unsigned    el_stride   ,
+        double *const     r_base   , const unsigned     r_stride   ,
+        double *const dist2_base   , const unsigned dist2_stride   ,
+  const double *const     x_base[2], const unsigned     x_stride[2],
+  const uint    *const  session_id_base, const unsigned session_id_stride,
+  const uint npt, struct findptsnn_local_data_2 *const fd,
+  buffer *buf);
+void findptsnn_local_eval_2(
+        double *const out_base, const unsigned out_stride,
+  const uint   *const  el_base, const unsigned  el_stride,
+  const double *const   r_base, const unsigned   r_stride,
+  const uint npt,
+  const double *const in, struct findptsnn_local_data_2 *const fd);
+
+#define findptsnn_local_setup_3   PREFIXED_NAME(findptsnn_local_setup_3)
+#define findptsnn_local_free_3    PREFIXED_NAME(findptsnn_local_free_3 )
+#define findptsnn_local_3         PREFIXED_NAME(findptsnn_local_3      )
+#define findptsnn_local_eval_3    PREFIXED_NAME(findptsnn_local_eval_3 )
+
+struct findptsnn_local_hash_data_3 {
+  uint hash_n;
+  struct dbl_range bnd[3];
+  double fac[3];
+  uint *offset;
+  uint max;
+};
+
+struct findptsnn_local_data_3 {
+  unsigned ntot;
+  const double *elx[3];
+  const int *session_id;
+  struct obbox_3 *obb;
+  struct findptsnn_local_hash_data_3 hd;
+  struct findptsnn_el_data_3 fed;
+  double tol;
+};
+
+void findptsnn_local_setup_3(struct findptsnn_local_data_3 *const fd,
+                           const double *const elx[3],
+                           const int *const session_id,
+                           const unsigned n[3], const uint nel,
+                           const unsigned m[3], const double bbox_tol,
+                           const uint max_hash_size,
+                           const unsigned npt_max, const double newt_tol);
+void findptsnn_local_free_3(struct findptsnn_local_data_3 *const fd);
+void findptsnn_local_3(
+        uint   *const  code_base   , const unsigned  code_stride   ,
+        uint   *const    el_base   , const unsigned    el_stride   ,
+        double *const     r_base   , const unsigned     r_stride   ,
+        double *const dist2_base   , const unsigned dist2_stride   ,
+  const double *const     x_base[3], const unsigned     x_stride[3],
+  const uint    *const  session_id_base, const unsigned session_id_stride,
+  const uint npt, struct findptsnn_local_data_3 *const fd,
+  buffer *buf);
+void findptsnn_local_eval_3(
+        double *const out_base, const unsigned out_stride,
+  const uint   *const  el_base, const unsigned  el_stride,
+  const double *const   r_base, const unsigned   r_stride,
+  const uint npt,
+  const double *const in, struct findptsnn_local_data_3 *const fd);
+
+#endif
