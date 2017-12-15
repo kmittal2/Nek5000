b93af7b7fd31d004834c2cce397e48b50e7249e0
Ketan Mittal <kmittal2@illinois.edu>
2017-12-15 15:22:41 -0600

    caling ortho univ in plan4

--- core/plan4.f
+++ core/plan4.f
@@ -126,7 +126,7 @@ c      See change in pressure from previous iteration
          if (nid.eq.0) 
      $      write(6,'(i2,i8,i4,1p2e13.4,a11)') idsess,istep,i,time,
      $      dprmax,' max-dp-nn'
-c         call ortho_univ2   (pr)
+         call ortho_univ2   (pr)
 c         call ortho_univ (pr)
          call modpresint('o  ','v  ')
 c           if (istep.gt.5) call outpost(vx,vy,vz,pr,t,'   ')
